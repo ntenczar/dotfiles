63f4167a13930e8a06355f682a20ea746cde4d4d
Nate Tenczar <ntenczar@gmail.com>
2017-12-11 12:50:52 -0500

    mo hotkeys

---
+++ screenlayout/work.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI1 --auto --primary --mode 3440x1440 --panning 4644x1944 --scale 1.35x1.35 --pos 0x0 --rotate normal --output VIRTUAL1 --off --output DP1 --off --output eDP1 --off --output DP2 --off
