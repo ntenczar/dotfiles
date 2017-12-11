3546c6c5a98e223545b678bb6d27186712eed9dc
Nate Tenczar <ntenczar@gmail.com>
2017-12-11 14:36:32 -0500

    work setup is mo betta

--- screenlayout/work.sh
+++ screenlayout/work.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-xrandr --output HDMI1 --auto --primary --mode 3440x1440 --panning 4644x1944 --scale 1.35x1.35 --pos 0x0 --rotate normal --output VIRTUAL1 --off --output DP1 --off --output eDP1 --off --output DP2 --off
+xrandr --output HDMI1 --auto --primary --mode 3440x1440 --panning 6020x2520 \
+  --scale 1.75x1.75 --pos 0x0 --rotate normal --output VIRTUAL1 --off \
+  --output DP1 --off --output eDP1 --off --output DP2 --off
+
+feh ~/Pictures/classic-wheels-3440x1440.jpg --bg-scale
